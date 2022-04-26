e879a31e3d6217d65aade3b5039dc0f68ac66b5d
CF Wang <cf.wang@metropia.com>
2022-04-26 14:54:44 +0800

    add gitsigns & treesitter

---
+++ lua/user/treesitter.lua
@@ -0,0 +1,22 @@
+-- folding: zo, zc
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
+
+configs.setup {
+configs.setup {
+    ensure_installed = { "go", "julia", "c", "rust" },
+    sync_install = false,
+    highlight = {
+        enable = true,
+        disable = { "" },
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true,
+    },
+}
