a715e12e62d08c0d1ba1ee72e4d9157b219f96be
CF Wang <cf.wang@metropia.com>
2022-04-27 19:10:14 +0800

    add new colorscheme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -52,6 +52,7 @@ return packer.startup(function(use)
     -- colorscheme
     use 'tomasr/molokai'
     use 'martinsione/darkplus.nvim'
+    use 'tanvirtin/monokai.nvim'
 
     -- git
     use 'lewis6991/gitsigns.nvim'
