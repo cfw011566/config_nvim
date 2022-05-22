16a494eff55d5ea3ad95500f7d6e9636be615c3f
CF Wang <cf.wang@metropia.com>
2022-05-22 10:33:16 +0800

    remove rust.vim and add autocmd

--- lua/user/rust-tools.lua
+++ lua/user/rust-tools.lua
@@ -1,9 +1,12 @@
---[[
 local status_ok, rust_tools = pcall(require, "rust-tools")
 if not status_ok then
     return
 end
 
+rust_tools.setup({})
+
+-- default settings
+--[[
 local opts = {
 	tools = { -- rust-tools options
 		-- automatically set inlay hints (type hints)
@@ -190,40 +193,4 @@ local opts = {
 
 rust_tools.setup(opts)
 
-local opts = {
-    tools = { -- rust-tools options
-        autoSetHints = true,
-        hover_with_actions = true,
-        inlay_hints = {
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
-        },
-    },
-
-    -- all the opts to send to nvim-lspconfig
-    -- these override the defaults set by rust-tools.nvim
-    -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
-    server = {
-        -- on_attach is a callback called when the language server attachs to the buffer
-        -- on_attach = on_attach,
-        settings = {
-            -- to enable rust-analyzer settings visit:
-            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-            ["rust-analyzer"] = {
-                -- enable clippy on save
-                checkOnSave = {
-                    command = "clippy"
-                },
-            }
-        }
-    },
-}
-
-require('rust-tools').setup(opts)
-
 --]]
-
-require('rust-tools').setup({})
-
--- require('rust-tools.runnables').runnables()
