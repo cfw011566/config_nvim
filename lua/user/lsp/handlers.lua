25fa86a53b17e8c7b99380452bde3895459dac8a
CF Wang <cf.wang@metropia.com>
2022-12-23 20:44:24 +0800

    vim.lsp.buf.format

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -84,8 +84,8 @@ local function lsp_keymaps(bufnr)
     -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<Leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
     vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-    vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format({ async = false })<CR>', opts)
+    vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format({ async = false })' ]]
 end
 
 M.on_attach = function(client, bufnr)
